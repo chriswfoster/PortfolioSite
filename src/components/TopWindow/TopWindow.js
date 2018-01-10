45c50241f1c07b3bb573ab8a5bae0967cd49648b
ChriswFoster <chriswfoster@gmail.com>
2018-01-10 17:35:16 -0600

    Fixed scrolling arrow

--- src/components/TopWindow/TopWindow.js
+++ src/components/TopWindow/TopWindow.js
@@ -1,21 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 
-import './topwindow.css'
-
-
+import "./topwindow.css"
 
 export default class TopWindow extends Component {
-
-
-    render(){
-        return(
-            <div className="window">
-            <div>
-        <p className="topname">Chris Foster</p>
-        <p>FULL STACK WEB DEVELOPER</p></div>
-        <img src={require('./arrow')} />
-                </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="window">
+        <div>
+          <p className="topname">Chris Foster</p>
+          <p>FULL STACK WEB DEVELOPER</p>
+        </div>
+        <img src={require("./arrow.png")} />
+      </div>
+    )
+  }
 }
