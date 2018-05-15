35d387d11de0bce4bb964d635bbc616317a47984
ChriswFoster <chriswfoster@gmail.com>
2018-05-15 10:51:34 -0500

    Inputs done

--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -1,30 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from "react"
 
 class ContactForm extends Component {
-    constructor(){
-        super()
-        this.state = {
-
-        }
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      company: "",
+      phoneNumber: "",
+      email: "",
+      message: ""
     }
+  }
 
+  handleText(e, type) {}
 
-
-
-    render(){
-        return(
-            <div>
-                <form>
-                    <input type="text" placeholder="Name"/>
-                    <input type="text" placeholder="Company (optional)"/>
-                    <input type="text" placeholder="Phone number"/>
-                    <input type="text" placeholder="Email" />
-                    <input type="text" placeholder="Type me a message" />
-                    <input type="submit" text="test" />
-                    </form>
-
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={e => this.handleText(e, "name")}
+          />
+          <input
+            type="text"
+            placeholder="Company (optional)"
+            onChange={e => this.handleText(e, "company")}
+          />
+          <input
+            type="text"
+            placeholder="Phone number"
+            onChange={e => this.handleText(e, "phoneNumber")}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={e => this.handleText(e, "email")}
+          />
+          <input
+            type="text"
+            placeholder="Type me a message"
+            onChange={e => this.handleText(e, "message")}
+          />
+          <input type="submit" text="test" />
+        </form>
+      </div>
+    )
+  }
 }
-export default ContactForm;
+export default ContactForm
